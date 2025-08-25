5b89f85c14655d43c085d67fa27d6cc75f831f04
Ganesh Kolagatla <ganeshkolagatla09@gmail.com>
2025-08-25 17:37:31 +0530

    commit befor pull

--- src/main/java/com/alacriti/inbound/serviceimpl/BatchDataPostProcessorImpl.java
+++ src/main/java/com/alacriti/inbound/serviceimpl/BatchDataPostProcessorImpl.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 
 import com.alacriti.inbound.service.IBatchDataPostProcessor;
@@ -15,22 +16,23 @@ import com.alacriti.inbound.service.IBatchDataPostProcessor;
 public class BatchDataPostProcessorImpl implements IBatchDataPostProcessor {
 
 	@Override
+	@PostMapping("/hitkafka")
 	public void postProcess() {
 		try {
 	        String xmlPayload = """
-	            <PaymentInstruction>
-	                <fileName>%s</fileName>
-	                <createdDate>%s</createdDate>
-	                <clientKey>%s</clientKey>
-	            </PaymentInstruction>
-	            """.formatted(fileName, createdDate, clientKey);
+	           <FileNotification>
+                 <fileName>monthly_report_august.pdf</fileName>
+                 <clientKey>ICICI001</clientKey>
+                 <date>2025-08-25</date>
+               </FileNotification>
+	            """.formatted();
 
 	        RestTemplate restTemplate = new RestTemplate();
 	        HttpHeaders headers = new HttpHeaders();
 	        headers.setContentType(MediaType.APPLICATION_XML);
 
 	        HttpEntity<String> request = new HttpEntity<>(xmlPayload, headers);
-	        String endpoint = "https://xyz.ngrok.io/send";
+	        String endpoint = "https://7ea3d690c952.ngrok-free.app/send-file-notification";
 	        ResponseEntity<String> response = restTemplate.postForEntity(endpoint, request, String.class);
 	        System.out.println("Response: " + response.getBody());
 	    } catch (Exception e) {
