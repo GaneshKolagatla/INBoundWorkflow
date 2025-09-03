971f9cd5fca1a9020ba87c32e2ce4d5333e0459e
vatsalyas <vatsalyas@vatsalyas-lp.ds.alacriti.com>
2025-09-03 12:14:49 +0530

    DONE.

--- src/main/java/com/alacriti/inbound/repository/DownloadEventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alacriti.inbound.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.alacriti.inbound.model.DownloadEvent;
-
-public interface DownloadEventRepository extends JpaRepository<DownloadEvent, Long> {
-
-}
